dccba990be817c4be1fe1bc81d329f353e12b09c
Keevin Aroco <gavikde2022@gmail.com>
2024-11-06 14:30:57 +0000

    Add function to create new grocery list and name it.

--- run.py
+++ run.py
@@ -12,9 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('grocery_list')
 
+print("Welcome to your Grocery list.")
 
-pantry = SHEET.worksheet('pantry')
 
-data = pantry.get_all_values()
+def create_new_list():
+    """
+    Add a worksheet for a new grocery list and name it based on user input.
+    """
+    new_list = input("Please enter a name for the list: ")
+    SHEET.add_worksheet(title = new_list, rows="100", cols="20")
 
-print(data)
+create_new_list()
